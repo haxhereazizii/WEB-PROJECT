f327503f9714a01dc78faf60fa89da99b9ed998c
haxhereazizii <nowayjerry22@gmail.com>
2024-04-27 21:48:02 +0300

    feat: implemented landing, login and register page

---
+++ admin_page.php
@@ -0,0 +1,42 @@
+<?php
+@include 'config.php';
+session_start();
+if(!isset( $_SESSION['admin_name'])){
+    header('location:login_form.php');
+}
+
+?>
+
+
+
+<!Doctype html>
+<html>
+<head> 
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> admin page </title>
+<!--custom css file -->
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <div class="container">
+
+    <div class="content">
+        <h3> hi, <span>   <?php echo $_SESSION['admin_name']; ?>
+        </span> </h3>
+        <h1> Welcome to  KOSOVO the wonderland!
+            <span> </span> <h1>
+            <p> This is an admin page </p>
+            <a href="login_form.php" class="btn">login</a>
+            <a href="register_form.php" class="btn">register</a>
+            <a href="logout.php" class="btn"> logout</a>
+
+
+        </div>
+        
+</body>
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>KOSOVO/WEBPROJECT</title>
+  <link rel="stylesheet" href="css/indexstyle.css">
+</head>
+<body>
+  <div class="banner"></div>
+  <div class="content">
+    <h1>COME AND VISIT KOSOVO!</h1>
+    <p>Register for more features, or log in if you already have an account.</p>
+    <div>
+    <a href="register_form.php"> <button type="button"><span></span>Register</button> </a>
+    <a href="login_form.php">  <button type="button"><span></span>Log in</button> </a>
+    </div>
+  </div>
+</body>
+</html>
