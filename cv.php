a2e0267214ebc22de5397842b5925c13b395d60a
haxhereazizii <nowayjerry22@gmail.com>
2024-04-28 18:04:37 +0300

    feat: created the cv part using html/css with my informations

--- cv.php
+++ cv.php
@@ -14,7 +14,7 @@ if(!isset(  $_SESSION['user_name'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/userpage.css">
+    <link rel="stylesheet" href="css/cv.css">
    <script defer src="activepage.js"> </script>
     <title>webpage</title>
 </head>
@@ -65,8 +65,71 @@ if(!isset(  $_SESSION['user_name'])){
     </div>
   </div>
 </nav>
+
+
+
+<div class="container">
+<div class="header">
+  <div class="img_area">
+    <img src="pixtures/5068379120 (2).jpg" alt="">
+  </div>
+  <h1>HAXHERE AZIZI</h1>
+  <h3>Computer engineering student</h3>
+</div>
+<div class="main">
+  <div class="left">
+    <h2>Contact information</h2>
+
+    <p> <strong>Email:</strong>haxhere.azizi24@gmail.com</p>
+    <p> <strong>Number:</strong>506 000 000</p>
+    <p> <strong>Adress:</strong>
+    Kosovo, Gjilan, Dobercan,
+Mehmet Ali Behluli Street
+
+  </p>
+    <h2>Skills </h2>
+    <ul>
+      <li>Programming languages</li>
+      <li> Project development</li>
+      <li> Content creation</li>
+      <li>Research</li>
+      <li>Presentation</li>
+  </ul>
+       <h2>Education</h2>
+       <h3> Sakarya Faculty of computer engineering and information sciences</h3>
+       <p> Major computer engineering (first year)- Türkiye </p>
+       <h3>Science high school “Xhavit Ahmeti”</h3>
+       <p>Graduated at 2023-Kosovo</p>
+  </div>
+  <div class="right">
+    <h2>Work experience</h2>
+    <h3> Career club of Xhavit Ahmeti</h3>
+    <p><strong>Position:</strong>Event organizer</p>
+  <ul>
+  <li>I organized sessions with professionals where young people
+exchange and got information about the job they were
+interested about
+ </li>
+  </ul>  <br>
+  <h3>Laboratori Yt </h3>
+  <p><strong>Position:</strong>Project manager</p>
+  <ul>
+  <li>I planned a schedule for implementation and managed 4000
+euros during the implementation process</li>
+  </ul> <br>
+  <h3> NGO TOKA </h3>
+    <p><strong>Position:</strong>Youth leader</p>
+  <ul>
+  <li> I contacted businesses
+in Kosovo for partnership in Solidarity action day in 2023, and represented Kosovo in actival a youth festival in Germany
+
+ </li>
+  </ul>
+  </div>
+</div>
 </div>
 
+</div>
 </body>
 </html>
 
